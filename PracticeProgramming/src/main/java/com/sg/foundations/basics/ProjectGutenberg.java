c1f9a49911bf2f1234d9c0a9a37cbe96cd9e87a2
Ihesinachi Nwankwo <ihe.nwk@hotmail.co.uk>
2022-10-13 13:14:29 +0100

    Completed exercises from java basics

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
@@ -0,0 +1,20 @@
+
+package com.sg.foundations.basics;
+
+/**
+ *Date: 12/10/2022
+ * @author Ihesinachi Nwankwo
+ * e-mail: ihe.nwk@hotmail.co.uk
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
+    
+}
